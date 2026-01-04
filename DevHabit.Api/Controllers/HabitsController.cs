f7c956a7d4102f51f30c73019d1ece6cac9c9bd8
mucnjakf <crofilix@gmail.com>
2026-01-04 02:38:56 +0100

    Implemented exception handling

--- DevHabit.Api/Controllers/HabitsController.cs
+++ DevHabit.Api/Controllers/HabitsController.cs
@@ -49,12 +49,7 @@ public sealed class HabitsController(ApplicationDbContext dbContext) : Controlle
         CreateHabitDto createHabitDto,
         IValidator<CreateHabitDto> validator)
     {
-        ValidationResult validationResult = await validator.ValidateAsync(createHabitDto);
-
-        if (!validationResult.IsValid)
-        {
-            return BadRequest(validationResult.ToDictionary());
-        }
+        await validator.ValidateAndThrowAsync(createHabitDto);
 
         Habit habit = createHabitDto.ToEntity();
 
---
+++ DevHabit.Api/Middleware/ValidationExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevHabit.Api.Middleware;
+
+public sealed class ValidationExceptionHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        if (exception is not ValidationException validationException)
+        {
+            return false;
+        }
+
+        httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        var context = new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Detail = "One or more validation errors occurred",
+                Status = StatusCodes.Status400BadRequest
+            }
+        };
+
+        var errors = validationException.Errors
+            .GroupBy(x => x.PropertyName)
+            .ToDictionary(
+                x => x.Key.ToLowerInvariant(),
+                x => x.Select(y => y.ErrorMessage).ToArray());
+
+        context.ProblemDetails.Extensions.Add("errors", errors);
+
+        return await problemDetailsService.TryWriteAsync(context);
+    }
+}
