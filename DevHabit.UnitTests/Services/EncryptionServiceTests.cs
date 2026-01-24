6fe7e83da8ab596ddca5ace4f182363bb3f3ac51
mucnjakf <crofilix@gmail.com>
2026-01-25 00:41:21 +0100

    Implemented unit tests

---
+++ DevHabit.UnitTests/Services/EncryptionServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using DevHabit.Api.Options;
+using DevHabit.Api.Services;
+using Microsoft.Extensions.Options;
+
+namespace DevHabit.UnitTests.Services;
+
+public sealed class EncryptionServiceTests
+{
+    private readonly EncryptionService _sut;
+
+    public EncryptionServiceTests()
+    {
+        IOptions<EncryptionOptions> encryptionOptions = Options.Create(new EncryptionOptions
+        {
+            Key = Convert.ToBase64String(RandomNumberGenerator.GetBytes(32))
+        });
+
+        _sut = new EncryptionService(encryptionOptions);
+    }
+
+    [Fact]
+    public void Decrypt_ShouldReturnPlainText_WhenDecryptingCorrectCiphertext()
+    {
+        const string plainText = "sensitive data";
+        string ciphertext = _sut.Encrypt(plainText);
+
+        string decryptedCiphertext = _sut.Decrypt(ciphertext);
+
+        Assert.Equal(plainText, decryptedCiphertext);
+    }
+}
---
+++ DevHabit.UnitTests/Validators/CreateEntryRequestValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using DevHabit.Api.Dtos.Entries;
+using FluentValidation.Results;
+
+namespace DevHabit.UnitTests.Validators;
+
+public sealed class CreateEntryRequestValidatorTests
+{
+    private readonly CreateEntryRequestValidator _sut = new();
+
+    [Fact]
+    public async Task ValidateAsync_ShouldSucceed_WhenInputRequestIsValid()
+    {
+        var request = new CreateEntryRequest
+        {
+            HabitId = $"h_{Guid.CreateVersion7()}",
+            Value = 1,
+            Date = DateOnly.FromDateTime(DateTime.UtcNow)
+        };
+
+        ValidationResult validationResult = await _sut.ValidateAsync(request);
+
+        Assert.True(validationResult.IsValid);
+        Assert.Empty(validationResult.Errors);
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ShouldFail_WhenHabitIdIsEmpty()
+    {
+        var request = new CreateEntryRequest
+        {
+            HabitId = string.Empty,
+            Value = 1,
+            Date = DateOnly.FromDateTime(DateTime.UtcNow)
+        };
+
+        ValidationResult validationResult = await _sut.ValidateAsync(request);
+
+        Assert.False(validationResult.IsValid);
+        ValidationFailure validationFailure = Assert.Single(validationResult.Errors);
+        Assert.Equal(nameof(CreateEntryRequest.HabitId), validationFailure.PropertyName);
+    }
+}
